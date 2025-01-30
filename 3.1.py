54637ff57c8964ae1fd7c07bdba447dd8a864cd3
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-01-30 09:37:21 +0100

    3.1

--- 3.1.py
+++ 3.1.py
@@ -1,9 +1,41 @@
 print("Vilket av denna tre abonnemang är du intreserad i?")
-print("Kontant (rekomenderad för högst 33 minuter av samtal per månad. Om du är intereserad skriv 1)")
-print("Normal (Rekomenderad för högst 66 minuter av samtal per månad. Om du är intereserad skriv 2)")
-print("Plus (Rekomenderad för dom som har mer än 66 minuter av samtal per månad. Om du är intereserad skriv 3)")
+print("Kontant: rekomenderad för högst 33 minuter av samtal per månad.")
+print("Normal: Rekomenderad för mellan 33 och 66 minuter av samtal per månad.")
+print("Plus: Rekomenderad för dom som har mer än 66 minuter av samtal per månad.")
+while True:
+    print("Upskatta hur många minuter du rigner per månad:")
+    användar_val = input("")
+    användar_val = int(användar_val)
 
-användar_val = input("")
 
-if användar_val == ("1"):
-    print("")
+    if användar_val <= 33:
+        print(f"Du upskattar att du kommer ringa {användar_val}")
+        print("Då är det bäst om du tar kontant.")
+
+        YN = input("Skriv 1 för att acceptera, 2 för att neka: ")
+        if YN == "1":
+            break
+        if YN == "2":
+            print("OK")
+
+    if användar_val > 33 and användar_val < 66:
+        print(f"Du upskattar att du kommer ringa {användar_val}")
+        print("Då är det bäst om du tar Normal.")
+
+        YN = input("Skriv 1 för att acceptera, 2 för att neka: ")
+        if YN == "1":
+            break
+        if YN == "2":
+            print("OK")
+
+    if användar_val >= 66:
+        print(f"Du upskattar att du kommer ringa {användar_val}")
+        print("Då är det bäst om du tar Plus.")
+
+        YN = input("Skriv 1 för att acceptera, 2 för att neka: ")
+        if YN == "1":
+            break
+        if YN == "2":
+            print("OK")
+
+print("Tack för att du valt ett abbonemang!")
